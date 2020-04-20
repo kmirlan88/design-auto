83e7306a25bddadc35afe0263f2589ead9e840a4
kai <kmirlan88@gmail.com>
2020-04-20 10:24:45 -0500

    init

---
+++ src/paperbase/Header.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import HelpIcon from '@material-ui/icons/Help';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import Link from '@material-ui/core/Link';
+import MenuIcon from '@material-ui/icons/Menu';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import Toolbar from '@material-ui/core/Toolbar';
+import Tooltip from '@material-ui/core/Tooltip';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const lightColor = 'rgba(255, 255, 255, 0.7)';
+
+const styles = (theme) => ({
+  secondaryBar: {
+    zIndex: 0,
+  },
+  menuButton: {
+    marginLeft: -theme.spacing(1),
+  },
+  iconButtonAvatar: {
+    padding: 4,
+  },
+  link: {
+    textDecoration: 'none',
+    color: lightColor,
+    '&:hover': {
+      color: theme.palette.common.white,
+    },
+  },
+  button: {
+    borderColor: lightColor,
+  },
+});
+
+function Header(props) {
+  const { classes, onDrawerToggle } = props;
+
+	const [value, setValue] = React.useState(0);
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
+
+  return (
+		<React.Fragment>
+			<AppBar color="primary" position="sticky" elevation={0}>
+				<Toolbar>
+					<Grid container spacing={1} alignItems="center">
+						<Hidden smUp>
+							<Grid item>
+								<IconButton
+									color="inherit"
+									aria-label="open drawer"
+									onClick={onDrawerToggle}
+									className={classes.menuButton}>
+									<MenuIcon />
+								</IconButton>
+							</Grid>
+						</Hidden>
+						<Grid item xs />
+
+						<Grid item>
+							<Tooltip title="Alerts • No alerts">
+								<IconButton color="inherit">
+									<NotificationsIcon />
+								</IconButton>
+							</Tooltip>
+						</Grid>
+						<Grid item>
+							<Button
+								className={classes.button}
+								variant="outlined"
+								color="inherit"
+								>
+								Login
+							</Button>
+						</Grid>
+					</Grid>
+				</Toolbar>
+			</AppBar>
+			<AppBar
+				component="div"
+				className={classes.secondaryBar}
+				color="primary"
+				position="static"
+				elevation={0}>
+				<Toolbar>
+					<Grid container alignItems="center" spacing={1}>
+						<Grid item xs>
+							<Typography color="inherit" variant="h5" component="h1">
+								HC Service
+							</Typography>
+						</Grid>
+					</Grid>
+				</Toolbar>
+			</AppBar>
+			<AppBar
+				component="div"
+				className={classes.secondaryBar}
+				color="primary"
+				position="static"
+				elevation={0}>
+				<Tabs value={value} textColor="inherit" onChange={handleChange}>
+					<Tab textColor="inherit" label="Template" />
+					<Tab textColor="inherit" label="Server" />
+					<Tab textColor="inherit" label="Cluster" />
+					<Tab textColor="inherit" label="Scaleup" />
+				</Tabs>
+			</AppBar>
+		</React.Fragment>
+	);
+}
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+  onDrawerToggle: PropTypes.func.isRequired,
+};
+
+export default withStyles(styles)(Header);
